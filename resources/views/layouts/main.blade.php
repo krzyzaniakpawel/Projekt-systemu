9b63e24e22b3a93d4aa94c63435dd4216644a9ca
Paweł Krzyżaniak <pkrzyzaniak02@gmail.com>
2025-06-14 13:25:20 +0200

    apache css fixes

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -4,7 +4,6 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>GolInfo</title>
-  <!-- <link href="style.css" rel="stylesheet"> -->
   <link href="{{ asset('style.css') }}" rel="stylesheet">
 
   <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600&display=swap" rel="stylesheet">
